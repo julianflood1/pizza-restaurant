2b24c5332c405f3b5852bd8a6fc6ae0a4d5b45ec
Julian Flood <Julian@Julians-MacBook-Pro.local>
2017-03-31 17:42:03 -0700

    add front end functionality, prototype for price not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
 //Business Logic
-function Pizza(size, topping) {
+function Pizza(size, toppingsTotal) {
   this.size = size;
-  this.topping = topping;
+  this.toppingsTotal = toppingsTotal;
 }
+
+
+Pizza.prototype.price = function() {
+  return this.size + this.toppingsTotal;
+}
+
 
 // Pizza.prototype.price = function() {
 //   var pizzaTotal = 0;
-//   (this.size + this.topping = pizzaTotal);
+//   (this.size + this.toppingsTotal = pizzaTotal);
 // }
 //Front-end Logic
 
@@ -17,11 +23,40 @@ $(document).ready(function() {
   $('form').submit(function(event) {
     event.preventDefault();
 
-    var toppings = $("input:checkbox[name=toppings]:checked").val();
-    console.log(toppings);
 
-    var sides = $("input:checkbox[name=sides]:checked").val();
-    console.log(sides);
+    var size = parseInt($("input[name='pizza-size']:checked").val());
+    console.log(size);
 
+    var toppings = [];
+     $("input:checkbox[name=toppings]:checked").each(function() {
+      var inputtedToppings = parseInt($(this).val());
+      toppings.push(inputtedToppings);
+    });
+    var toppingsTotal = 0;
+    for(i = 0; i < toppings.length; i++){
+      toppingsTotal += toppings[i]
+    }
+
+console.log(toppingsTotal);
+
+    var sides = [];
+     $("input:checkbox[name=sides]:checked").each(function() {
+      var inputtedSides = parseInt($(this).val());
+      sides.push(inputtedSides);
+    });
+    var sidesTotal = 0;
+    for(i = 0; i < sides.length; i++){
+      sidesTotal += sides[i]
+    }
+    console.log(sidesTotal);
+
+
+    newPizza = new Pizza(size, toppingsTotal);
+    console.log(newPizza.price);
+
+    // var foodTotal = sides + size + toppingsTotal;
+    $('#totalCost').text();
   });
+
+
 });
